359684bc01d258aec380dc6b9102ae78a60d038d
ZuperZee <ole.kristian.kaaseth@gmail.com>
2023-09-24 20:27:36 +0200

    chore(example): update tcp-client example

--- examples/tcp-client.rs
+++ examples/tcp-client.rs
@@ -1,34 +1,31 @@
 use std::{
     io::{Read, Write},
-    net::{SocketAddr, TcpStream}, time::Duration,
+    net::{SocketAddr, TcpStream},
+    time::Duration,
 };
 
-use modbus::adu::tcp::response::Response;
+use modbus::{
+    adu::tcp::{request::Request as AduRequest, response::Response as AduResponse},
+    pdu::request::Request as PduRequest,
+};
 
 fn main() {
     let addr: SocketAddr = "127.0.0.1:5502".parse().unwrap();
     let mut stream = TcpStream::connect(addr).unwrap();
-    stream.set_read_timeout(Some(Duration::from_secs(5))).unwrap();
-    let transaction_id: u16 = 1;
-    let protocol_id: u16 = 0;
-    let unit_id: u8 = 1;
-    let pdu = [0x4, 0x0, 0x0, 0x0, 0x7d];
-    let length: u16 = (pdu.len() + 1).try_into().unwrap();
-    let buf: &[u8] = &[
-        transaction_id.to_be_bytes().as_ref(),
-        protocol_id.to_be_bytes().as_ref(),
-        length.to_be_bytes().as_ref(),
-        &[unit_id],
-        &pdu,
-    ]
-    .concat();
-    stream.write_all(buf).unwrap();
+    stream
+        .set_read_timeout(Some(Duration::from_secs(5)))
+        .unwrap();
+    let pdu_req = PduRequest::ReadInputRegisters(0, 0x7d);
+    let req = AduRequest::new(1, 1, pdu_req);
+    let mut req_buf = vec![0_u8; req.adu_len()];
+    req.encode(&mut req_buf).unwrap();
+    stream.write_all(&req_buf).unwrap();
 
     let mut res_buf: [u8; 4096] = [0; 4096];
     let bytes_read = stream.read(&mut res_buf).unwrap();
     let res_buf = &res_buf[0..bytes_read];
-    let res = Response::try_from(res_buf).unwrap();
-    println!("{:?}", buf);
+    let res = AduResponse::try_from(res_buf).unwrap();
+    println!("{:?}", req_buf);
     println!("{:?}", res_buf);
     println!("{:?}", bytes_read);
     println!("{:?}", res);
